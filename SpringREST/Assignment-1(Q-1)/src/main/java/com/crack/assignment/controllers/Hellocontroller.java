1dec6f327a58616c978992fe9ca09d28807353f2
aniket-a-pandey <77108290+aniket-a-pandey@users.noreply.github.com>
2021-03-31 23:42:26 +0530

    Add files via upload

---
+++ SpringREST/Assignment-1(Q-1)/src/main/java/com/crack/assignment/controllers/Hellocontroller.java
@@ -0,0 +1,14 @@
+package com.crack.assignment.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hellocontroller {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+	
+}

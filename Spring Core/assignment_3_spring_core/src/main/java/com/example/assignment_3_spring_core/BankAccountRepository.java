49db5dd6f0798ab0b2e965f2da56306e5955d3a0
aniket-a-pandey <77108290+aniket-a-pandey@users.noreply.github.com>
2021-03-31 17:28:31 +0530

    Delete Spring Core directory

--- Spring Core/assignment_3_spring_core/src/main/java/com/example/assignment_3_spring_core/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.assignment_3_spring_core;
-
-public interface BankAccountRepository {
-
-    public double getBalance(long accountID);
-
-    public double updateBalance(long accountID, double newBalance);
-
-}
